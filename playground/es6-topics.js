f51abbd306f737e6ebad9ba94bfae03ed443c8d9
synergy2411 <synergy2411@outlook.com>
2020-01-14 13:03:42 +0530

    rest/spread

--- playground/es6-topics.js
+++ playground/es6-topics.js
@@ -35,8 +35,8 @@
 // }
 
 
-// function demo(){
-//     console.log(arguments);
+// function demo(...args){
+//     console.log();
 // }
 
 // demo("Foo", 32)
@@ -83,23 +83,149 @@
 // console.log("Promise is working...")
 
 
+// Producer Code
+// var delay = ms => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Here the data comes.");
+//             // reject(new Error('Bad Luck.'))
+//             // throw "Bad Luck";
+//         }, ms)
+//     })
+// }
 
-var delay = ms => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Here the data comes.")
-        }, ms)
+// // Consumer Code using then() function
+// delay(2000)
+//     .then(resp => console.log("SERVER RESPONSE : " + resp))
+//     .catch(err=>console.log(err));
+
+// // Consumer Code using Async ... await
+// async function callDelay() {
+//     try {
+//         var data = await delay(2000);
+//         console.log("ASYNC FUNCTION :", data)
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+
+// callDelay();
+
+
+
+
+
+
+
+// BLOCK SCOPING
+
+// function display(arr){
+//     if(arr.length < 2){
+//         let flash = ;
+//         // console.log(load);              // undefined
+//     }else{
+
+//         let load = "LOADING";
+//         console.log(load);
+//     }
+// }
+// display([2,3,4,5])
+
+
+
+
+//  IIFE
+
+
+
+// (function(){
+//     var myVar = ""
+//     console.log("IIFE Called");
+// })();
+
+
+// let funcArray = [];
+
+// for (var i = 0; i < 5; i++) {
+//     (function (num) {
+//         funcArray.push(function () {
+//             return num;
+//         })
+//     })(i)
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     funcArray.push(function(){return i;});
+// }
+
+// console.log(funcArray.length);
+// console.log(funcArray[3]());          // ?
+// console.log(funcArray[1]());          // ?
+// console.log(funcArray[4]());          // ?
+
+
+// const user = "FOO";
+
+// user = "Bar";
+
+
+// const user = Object.freeze({
+//     name : "Foo"
+// })
+
+// user.name = "Bar";
+
+// console.log(user);
+
+
+
+
+
+
+// REST / SPREAD
+
+// function display(name, ...args){
+//     console.log(args[1]);              // 
+//     for(let item of args){
+//         if(typeof item === 'number'){
+//             console.log(item + " is number.");
+//         }
+//     }
+// }
+
+// display("Foo");
+// display("Foo", 32);
+// display("Foo", 34, 'test@test.com');
+
+
+
+// SPREAD
+let arr = [3,4,5];
+
+let newArr = [1,2,arr];
+
+// console.log(newArr);            // ? [1,2,[3,4,5]]      [1,2,3,4,5]
+
+
+let obj = {
+    firstName : "Foo",
+    lastName : "Bar"
+}
+
+let newObj = {
+    ...obj,
+    age : 32
+}
+
+obj.lastName = "Changed Name";
+
+// console.log(newObj);
+
+
+let targetObj = Object.assign({}, obj, {
+        email : 'test@test.com', 
+        firstName : "New F Name"
     })
-}
-// Consumer Code
-delay(2000).then(resp => console.log("SERVER RESPONSE : " + resp));
 
-// Consumer Code by Async ... await
-async function callDelay(){
-    var data = await delay(2000);
-    console.log("ASYNC FUNCTION :", data)
-}
-
-callDelay();
-
+console.log(targetObj);
 
